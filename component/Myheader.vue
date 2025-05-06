27e8309c426256fef6686a5d3beb4d81017a4ae3
reddy-tech <59339052+reddy-tech@users.noreply.github.com>
2025-05-06 18:40:52 -0400

    Create Myheader.vue

---
+++ component/Myheader.vue
@@ -0,0 +1,31 @@
+<template>
+
+
+    <h2> {{ bookName }}</h2>
+    
+  </template>
+  
+  <script>
+  
+  
+   
+  
+  export default {
+    name: 'MyHeader',
+    props: {
+        bookName: String,
+        
+    }
+    
+  }
+  
+  
+  
+  
+  </script>
+  
+  
+  <style>
+  
+  
+  </style>
